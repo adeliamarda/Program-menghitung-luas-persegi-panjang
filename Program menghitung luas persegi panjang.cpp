9823eecf87499a1969074e8eaa82afac084dc7a0
Adelia Marda Trisno <46318729+adeliamarda@users.noreply.github.com>
2019-01-03 19:29:37 +0700

    Add files via upload

---
+++ Program menghitung luas persegi panjang.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    float p,l,luas;
+    cout<<"masukan panjang persegi panjang:";
+    cin>>p;
+    cout<<"masukan lebar persegi panjang:";
+    cin>>l;
+    luas=p*l;
+    cout<<"luas persegi panjang adalah"<<luas;
+    return 0;
+}
